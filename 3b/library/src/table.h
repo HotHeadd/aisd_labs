cab658b39b8f497ba380f6fd177b19852e631bd3
dotsenko.va <vdocenko439@gmail.com>
2024-04-02 22:49:16 +0300

    added extending and insertion

--- 3b/library/src/table.h
+++ 3b/library/src/table.h
@@ -6,15 +6,20 @@
 #define ELEM_NOT_FOUND 3
 #define FILE_ERROR 4 
 #define FORMAT_ERROR 5
-#define END_INPUT -1
 #define NO_TABLE 6
-#define EOTABLE 10
-#define LAST_ELEMENT 11
+#define EOTABLE 7
+#define LAST_ELEMENT 8
+#define END_INPUT -1
+
 typedef struct KeySpace {
     // ключ элемента
-    unsigned int key;
+    unsigned key;
+    // номер версии элемента
+    unsigned release;
     // указатель на информацию
-    char *info;
+    unsigned *info;
+    // указатель на следующий элемент
+    struct KeySpace *next;
 } KeySpace;
 
 typedef struct table_t {
@@ -25,39 +30,15 @@ typedef struct table_t {
     int csize;
 } table_t;
 
-typedef struct iterator_t{
-    KeySpace* current;
-} iterator_t;
-
-// TODO: CONST DATA TYPE WHERE REQUIRED
 void display(const table_t* table);
 table_t* get_table(const int size);
-void free_table(table_t * const table); // нужно ли писать так?
+void free_table(table_t * const table);
 
 int to_text(const table_t* table, const char*);
 int from_text(table_t** table, const char* filename);
 
-int delete_interval(table_t* table, const int highest, const int lowest);
-
-#ifdef MAIN_TASK
-int binsearch\
-(void* data, const void* inserted, const int amount, const int size, int(*copmarator)(const void*, const void*));
-KeySpace* find(const table_t*, const unsigned int);
+KeySpace* find(const table_t*, const unsigned);
 void print_found(const KeySpace*);
 void free_elem(KeySpace* elem);
-int insert(table_t* table, char* info, unsigned int key);
+int insert(table_t** table, unsigned info, unsigned);
 int delete(table_t*, const unsigned int);
-#elif DOP_TASK
-int iter_compare(const iterator_t first, const iterator_t second);
-iterator_t begin(const table_t* table);
-iterator_t end(const table_t* table);
-iterator_t next(const iterator_t iter);
-iterator_t back(const iterator_t iter);
-KeySpace* iter_pointer(const iterator_t iter);
-unsigned int iter_key(const iterator_t iter);
-char* iter_value(const iterator_t iter);
-iterator_t find(const table_t *table, const unsigned int key, int* res);
-void print_found(const iterator_t iter);
-iterator_t insert(table_t* table, char* info, unsigned int key, int* res);
-iterator_t delete(table_t*, const unsigned int, int* res);
-#endif
--- 3b/src/app.c
+++ 3b/src/app.c
@@ -16,6 +16,25 @@ int eXXit(int mistake, table_t* table, char* filename){
 
 }
 
+int ask_key(unsigned *key){
+    printf("Введите ключ: ");
+    int res = custom_int_input(key, o_n_and_0);
+    if (res == -1) return END_INPUT;
+    return GOOD;
+}
+
+
+int ask_elem(unsigned* key, unsigned* info){
+    printf("Введите ключ: ");
+    int res = custom_int_input(key, o_n_and_0);
+    if (res == -1) return END_INPUT;
+    printf("Введите значение: ");
+    res = custom_int_input(info, o_n_and_0);
+    if (res == -1) return END_INPUT;
+    return GOOD;
+}
+
+
 // НЕ КОНЧАЕМ РАБОТУ ДО КОНТРОЛ Д
 
 void menus(){
@@ -27,43 +46,27 @@ void menus(){
     printf("(5) Найти элемент в таблице\n");
     printf("(6) Записать таблицу в текстовый файл\n");
     printf("(7) Получить таблицу из текстового файла\n");
-    printf("(8) Индивидуальное задание (удалить элементы в диапозоне ключей\n");
+    printf("(8) Индивидуальное задание 1 -- поиск по ключу или версии\n");
+    printf("(9) Индивидуальное задание 2 -- реорганизация таблицы\n");
     printf("--> ");
-}
-
-int ask_elem(char** info, unsigned int* key){
-    printf("Введите ключ: ");
-    int res = custom_int_input(key, o_n_and_0);
-    if (res == -1) return END_INPUT;
-    *info = readline("Введите строку (значение): ");
-    if (*info == NULL) return END_INPUT;
-    return GOOD;
-}
-
-int ask_key(unsigned int *key){
-    printf("Введите значение ключа: ");
-    int res = custom_int_input(key, o_n_and_0);
-    if (res == -1) return END_INPUT;
-    return GOOD;
 }
 
 int main(){
     char choice;
     void* ret;
-    unsigned int key;
-    char* info;
+    unsigned key;
+    unsigned info;
     int res;
     char* filename = NULL;
     int size;
     table_t* table = NULL;
     menus();
-    #ifdef MAIN_TASK
     while ((choice = better_getchar()) != EOF){
         switch(choice){
             case '1':
                 free_table(table);
                 printf("Введите размер таблицы: ");
-                res = custom_int_input(&size, o_n_and_0);
+                res = custom_int_input(&size, only_negative);
                 if (res == -1){
                     return 0;
                 }
@@ -73,16 +76,14 @@ int main(){
                 display(table);
                 break;
             case '3':
-                res = ask_elem(&info, &key);
+                res = ask_elem(&key, &info);
                 if (res == -1) return eXXit(GOOD, table, filename);
-                res = insert(table, info, key);
-                if (res != GOOD) free(info);
+                res = insert(&table, info, key);
                 if (res == NO_TABLE) printf("Таблицы нет!\n");
-                if (res == KEY_EXIST) printf("Элемент с таким ключем уже есть!\n");
                 if (res == TABLE_OVERFLOW) printf("Таблица переполнена!\n");
                 break;
             case '4':
-                res = ask_key(&key);
+                //res = ask_key(&key);
                 if (res == -1) return eXXit(GOOD, table, filename);
                 res = delete(table, key);
                 if (res != GOOD){
@@ -92,7 +93,7 @@ int main(){
                 else printf("Элемент удален.\n");
                 break;
             case '5':
-                res = ask_key(&key);
+                //res = ask_key(&key);
                 if (res == -1) return eXXit(GOOD, table, filename);
                 ret = find(table, key);
                 if (ret == NULL) printf("Элемент не найден.\n");
@@ -105,7 +106,7 @@ int main(){
                 free(filename);
                 filename = readline("Введите имя файла, в который нужно поместить таблицу: ");
                 if (filename == NULL) return eXXit(GOOD, table, filename);
-                res = to_text(table, filename);
+                //res = to_text(table, filename);
                 if (res != GOOD){
                 if (res == NO_TABLE) printf("Таблицы нет!\n");
                 if (res == FILE_ERROR) printf("Ошибка файла\n");
@@ -116,7 +117,7 @@ int main(){
                 free(filename);
                 filename = readline("Введите имя файла, из кторого взять таблицу: ");
                 if (filename == NULL) return eXXit(GOOD, table, filename);
-                res = from_text(&table, filename);
+                //res = from_text(&table, filename);
                 if (res == FILE_ERROR) printf("Ошибка файла\n");
                 else if (res == FORMAT_ERROR)
                 {
@@ -125,120 +126,12 @@ int main(){
                 else printf("Таблица записана\n");
                 break;
             case '8':
-                int highest, lowest;
-                printf("Введите нижний ключ: ");
-                res = custom_int_input(&lowest, only_negative);
-                if (res == -1) return eXXit(GOOD, table, filename);
-                printf("Введите верхний ключ: ");
-                res = custom_int_input(&highest, only_negative);
-                if (res == -1) return eXXit(GOOD, table, filename);
-                while (highest < lowest){
-                    printf("Неправильный верхний ключ!\n");
-                    printf("Введите верхний ключ: ");
-                    res = custom_int_input(&highest, only_negative);
-                    if (res == -1) return eXXit(GOOD, table, filename);
-                }
-                res = delete_interval(table, highest, lowest);
+                // indie
                 break;
             default:
                 printf("Нет такой опции в меню!\n");
         }
         menus();
     }
-    #elif DOP_TASK
-    iterator_t iter;
-    while ((choice = better_getchar()) != EOF){
-        switch(choice){
-            case '1':
-                free_table(table);
-                printf("Введите размер таблицы: ");
-                res = custom_int_input(&size, o_n_and_0);
-                if (res == -1){
-                    return 0;
-                }
-                table = get_table(size);
-                break;
-            case '2':
-                display(table);
-                break;
-            case '3':
-                res = ask_elem(&info, &key);
-                if (res == -1) return eXXit(GOOD, table, filename);
-                iter = insert(table, info, key, &res);
-                if (res != GOOD){
-                    free(info);
-                if (res == NO_TABLE) printf("Таблицы нет!\n");
-                if (res == KEY_EXIST) printf("Элемент с таким ключем уже есть!\n");
-                if (res == TABLE_OVERFLOW) printf("Таблица переполнена!\n");
-                }
-                else printf("Итератор на элемент %u \"%s\"\n", iter_key(iter), iter_value(iter));
-                break;
-            case '4':
-                res = ask_key(&key);
-                if (res == -1) return eXXit(GOOD, table, filename);
-                iter = delete(table, key, &res);
-                if (res != GOOD){
-                if (res == ELEM_NOT_FOUND) printf("Элемент не найден.\n");
-                if (res == NO_TABLE) printf("Таблицы нет!\n");
-                }
-                else{
-                    printf("Элемент удален.\n");
-                    if (res == LAST_ELEMENT) printf("Вы удалили последный элемент\n");
-                    else printf("Итератор на элемент %u \"%s\"\n", iter_key(iter), iter_value(iter));
-                }
-                break;
-            case '5':
-                res = ask_key(&key);
-                if (res == -1) return eXXit(GOOD, table, filename);
-                iter = find(table, key, &res);
-                if (res == ELEM_NOT_FOUND) printf("Элемент не найден.\n");
-                else {
-                    print_found(iter);
-                }
-                break;
-            case '6':
-                free(filename);
-                filename = readline("Введите имя файла, в который нужно поместить таблицу: ");
-                if (filename == NULL) return eXXit(GOOD, table, filename);
-                res = to_text(table, filename);
-                if (res != GOOD){
-                if (res == NO_TABLE) printf("Таблицы нет!\n");
-                if (res == FILE_ERROR) printf("Ошибка файла\n");
-                }
-                else printf("Таблица записана\n");
-                break;
-            case '7':
-                free(filename);
-                filename = readline("Введите имя файла, из кторого взять таблицу: ");
-                if (filename == NULL) return eXXit(GOOD, table, filename);
-                res = from_text(&table, filename);
-                if (res != GOOD){
-                if (res == FILE_ERROR) printf("Ошибка файла\n");
-                if (res == FORMAT_ERROR)  printf("Ошибка формата данных\n");
-                }
-                else printf("Таблица записана\n");
-                break;
-            case '8':
-                int highest, lowest;
-                printf("Введите нижний ключ: ");
-                res = custom_int_input(&lowest, o_n_and_0);
-                if (res == -1) return eXXit(GOOD, table, filename);
-                printf("Введите верхний ключ: ");
-                res = custom_int_input(&highest, o_n_and_0);
-                if (res == -1) return eXXit(GOOD, table, filename);
-                while (highest < lowest){
-                    printf("Неправильный верхний ключ!\n");
-                    printf("Введите верхний ключ: ");
-                    res = custom_int_input(&highest, o_n_and_0);
-                    if (res == -1) return eXXit(GOOD, table, filename);
-                }
-                delete_interval(table, highest, lowest);
-                break;
-            default:
-                printf("Нет такой опции в меню!\n");
-        }
-        menus();
-    }
-    #endif
     return eXXit(GOOD, table, filename);
 }
