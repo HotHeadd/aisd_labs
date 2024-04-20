2e87060cd999a22db595521ac360d7822c0a2bf5
dotsenko.va <vdocenko439@gmail.com>
2024-04-20 17:24:15 +0300

    Started timer

--- 4a/src/timer.c
+++ 4a/src/timer.c
@@ -1,6 +1,25 @@
 #include "../library/src/tree.h"
 #include "../library/src/tree_io.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
-    return 0;
+    char* key;
+    unsigned info;
+    int elems;
+    Node* root = NULL;
+    double time_sum;
+    clock_t start, end;
+    for (int func=0; func<5; func++){
+        for (int i=1; i<=20; i++){
+            elems = i*1000;
+            for (int j = 0; j<elems; j++){
+                key = get_key();
+                info = get_info();
+                insert(&root, key, info);
+            }
+
+        }
+    }
 }
