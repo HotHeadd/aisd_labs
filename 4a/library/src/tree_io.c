06dcde39dfc092c8d73a772702b45cf9115439c0
dotsenko.va <vdocenko439@gmail.com>
2024-04-20 16:50:44 +0300

    Changed makefile

--- 4a/library/src/tree_io.c
+++ 4a/library/src/tree_io.c
@@ -103,6 +103,41 @@ void print_gv(Node* root){
     char* link = "https://dotrend.dozen.mephi.ru/?dot=digraph%20G%20%7B";
     char* example = "https://dotrend.dozen.mephi.ru/?dot=digraph%20G%20%7B%22A%20asd%22-%3EB%7D";
     char* end = "%7D";
-    printf("%s\n", link);
+    printf("%s\n", example);
+
+    // #define NO_LAYOUT_OR_RENDERING
+    
+    // int main(void) {
+    // #ifndef NO_LAYOUT_OR_RENDERING
+    // // set up a graphviz context - but only once even for multiple graphs
+    // GVC_t *gvc = gvContext();
+    // #endif
+
+    // // Create a simple digraph
+    // Agraph_t *g = agopen("g", Agdirected, 0);
+    // Agnode_t *n = agnode(g, "n", 1);
+    // Agnode_t *m = agnode(g, "m", 1);
+    // Agedge_t *e = agedge(g, n, m, 0, 1);
+
+    // // Set an attribute - in this case one that affects the visible rendering
+    // agsafeset(n, "color", "red", "");
+
+    // #ifdef NO_LAYOUT_OR_RENDERING
+    // // Just write the graph without layout
+    // agwrite(g, stdout);
+    // #else
+    // // Use the directed graph layout engine
+    // gvLayout(gvc, g, "dot");
+
+    // // Output in .dot format
+    // gvRender(gvc, g, "dot", stdout);
+
+    // gvFreeLayout(gvc, g);
+    // #endif
+
+    // agclose(g);
+
+    // return EXIT_SUCCESS;
+    // }
     return;
 }
---
+++ 4a/src/timer.c
@@ -0,0 +1,6 @@
+#include "../library/src/tree.h"
+#include "../library/src/tree_io.h"
+
+int main(){
+    return 0;
+}
